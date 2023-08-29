eb1346d1c759a7c8c40d1ceb5301f7ad2df5f6f4
AlexRazor <alexrazor1337@gmail.com>
2023-08-29 18:46:12 +0300

    Add pg url option

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,15 +14,25 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ReportsModule,
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        database: configService.get<string>('DB_NAME'),
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      }),
+      useFactory: async (configService: ConfigService) => {
+        if (configService.get<string>('DATABASE_URL')) {
+          return {
+            type: 'postgres',
+            url: configService.get<string>('DATABASE_URL'),
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          };
+        } else {
+          return {
+            type: 'postgres',
+            database: configService.get<string>('DB_NAME'),
+            host: configService.get<string>('DB_HOST'),
+            port: configService.get<number>('DB_PORT'),
+            username: configService.get<string>('DB_USERNAME'),
+            password: configService.get<string>('DB_PASSWORD'),
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          };
+        }
+      },
     }),
   ],
   controllers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 import { setupApp } from './setup-app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupApp(app);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT');
+  await app.listen(PORT);
 }
 bootstrap();
